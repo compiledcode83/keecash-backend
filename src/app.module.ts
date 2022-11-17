c80ec1347162012675f63fab94cf1658efde24b9
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2022-11-17 03:17:38 +0100

    Set global config and connect postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvHelper } from './common/helpers/env.helper';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import { validate } from './common/validators/env.validator';
+
+EnvHelper.verifyNodeEnv();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: EnvHelper.getEnvFilePath(),
+      isGlobal: true,
+      load: [appConfig, databaseConfig, jwtConfig],
+      validate: validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const config = configService.get('databaseConfig');
+        return {
+          ...config,
+          namingStrategy: new SnakeNamingStrategy(),
+          autoLoadEntities: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
