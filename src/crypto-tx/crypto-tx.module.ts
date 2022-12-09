54616ca74122ec8083ffda0e8de9f3b30bdab18c
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2022-12-09 08:43:48 +0100

    Fix cypto-tx module

--- src/crypto-tx/crypto-tx.module.ts
+++ src/crypto-tx/crypto-tx.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { CryptoTxController } from './crypto-tx.controller';
 
 @Module({
-  controllers: [CryptoTxController]
+  controllers: [CryptoTxController],
 })
 export class CryptoTxModule {}
