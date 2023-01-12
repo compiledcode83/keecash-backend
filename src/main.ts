e09aa3ce36ccac28f793bb4bcbce755d3c653a0e
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2023-01-12 03:26:46 +0100

    Fix swagger document name for our app

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Keekash API')
-    .setDescription('The Keekash API documentation')
+    .setTitle('Keecash API')
+    .setDescription('The Keecash API documentation')
     .setVersion('1.0')
     .addBearerAuth()
     .addServer(process.env.SWAGGER_SERVER_URL)
