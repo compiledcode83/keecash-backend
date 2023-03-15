bfa32b98166608f735aee889a7ac8c8f9f9b9ebc
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2023-03-15 19:37:16 +0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'return' }],
-  },
+  }
 };
--- src/api/country/country-activation/country-activation.module.ts
+++ src/api/country/country-activation/country-activation.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { CountryActivationService } from './country-activation.service';
 
 @Module({
-  providers: [CountryActivationService]
+  providers: [CountryActivationService],
 })
 export class CountryActivationModule {}
