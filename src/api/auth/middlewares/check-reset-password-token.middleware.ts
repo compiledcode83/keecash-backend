83cdc86f7c5790cfc2e16fe7be5ec96f6fa32e2c
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2023-04-06 17:34:18 +0100

    refactor: Add middleware for validating bearer token

---
+++ src/api/auth/middlewares/check-reset-password-token.middleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { CipherTokenService } from '@src/api/cipher-token/cipher-token.service';
+import { TokenTypeEnum } from '@src/api/cipher-token/cipher-token.types';
+
+@Injectable()
+export class CheckCreateAccountToken implements NestMiddleware {
+  constructor(private cipherTokenService: CipherTokenService) {}
+
+  async use(req: any, res: any, next: () => void) {
+    if (!req.headers.authorization) {
+      throw new UnauthorizedException('Missing reset-password token in the header');
+    }
+
+    const bearerResetPasswordToken = req.headers.authorization.split(' ')[1];
+
+    const token = await this.cipherTokenService.findOneBy({
+      token: bearerResetPasswordToken,
+      type: TokenTypeEnum.ResetPassword,
+    });
+
+    if (!token) {
+      throw new UnauthorizedException('Token is invalid');
+    }
+
+    req.token = token;
+    next();
+  }
+}
